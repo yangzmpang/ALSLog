d2b5ef51fce380377f886037960bdebd28d1c5a6
zimin.yzm <zimin.yzm@alibaba-inc.com>
2017-10-12 11:35:27 +0800

    no message

--- ALSLog.podspec
+++ ALSLog.podspec
@@ -39,6 +39,6 @@ Pod::Spec.new do |s|
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
   # s.dependency 'AFNetworking', '~> 2.3'
-s.dependency 'CocoaLumberjack'
+s.dependency 'CocoaLumberjack','3.2.1'
 s.dependency 'Sentry', '3.8.1'
 end
--- Example/Pods/Target Support Files/Pods-ALSLog_Example/Pods-ALSLog_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-ALSLog_Example/Pods-ALSLog_Example-frameworks.sh
@@ -91,11 +91,13 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/ALSLog/ALSLog.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CocoaLumberjack/CocoaLumberjack.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/KSCrash/KSCrash.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Sentry/Sentry.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/ALSLog/ALSLog.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CocoaLumberjack/CocoaLumberjack.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/KSCrash/KSCrash.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Sentry/Sentry.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
